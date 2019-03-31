617cb78dbdcc1a04918408e02187d41635831fcb
Magdalena <magmarmich@gmail.com>
2019-03-31 12:54:58 +0200

    Computer guesses the number

--- excercise_3.py
+++ excercise_3.py
@@ -1,16 +1,16 @@
 print("Pomyśl liczbę od 0 do 1000 a ja ją zgadne w max 10 próbach")
 min = 0
 max = 1000
-liczba_uzytkownika = True
-guess = int((max - min) / 2) + min
-print("Zgaduję " + str(guess))
-reakcja = int(input('Wpisz: 1) Za dużo 2) Za mało 3) Wygrałeś'))
-while not liczba_uzytkownika:
-    if int(reakcja == 3):
-        print("Wygrałem")
-    if int(reakcja) == 1:
-        guess = max
+
+while True:
+    guess = int((max - min) / 2) + min
+    print(guess)
+    num = int(input('Wpisz: 1) Za dużo 2) Za mało 3) Wygrałeś'))
+    if num == 1:
+        max = guess
+    elif num == 2:
+        min = guess
         print("Zgaduję " + str(guess))
-    elif int(reakcja == 1):
-        guess = min
-        print("Zgaduję " + str(guess))
+    else:
+        print("The computer guessed", guess, "and it was correct!")
+        break
